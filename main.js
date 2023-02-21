2c9f7e6729d0e034b6f9ba442b7a8c2ec5bd2030
WildJOKER85 <68331205+WildJOKER85@users.noreply.github.com>
2023-02-21 23:32:25 +0400

    Add files via upload
    added thisBindingTasks

---
+++ main.js
@@ -0,0 +1,73 @@
+// default binding
+// const obj = {
+//     method1: function () {
+//         return this
+//     },
+//     method2: () => {
+//         return this
+//     }
+// };
+//
+// const obj2 = {
+//     name: "Gor",
+//     age: 17,
+// };
+//
+// console.log(obj.method2())
+
+
+
+// Implicit binding
+// const obj = {
+//     method1: function () {
+//         return this.name
+//     },
+//     method2: () => {
+//         return this
+//     }
+// };
+//
+// const obj2 = {
+//     name: "Gor",
+//     age: 17,
+// };
+//
+// console.log(obj.method1.call(obj2))
+
+
+
+// Explicit binding
+// const obj = {
+//     method1: function () {
+//         return this.name
+//     },
+//     method2: () => {
+//         return this
+//     }
+// };
+//
+// const obj2 = {
+//     name: "Gor",
+//     age: 17,
+// };
+//
+// console.log(obj.method2())
+
+
+
+// new binding
+// const obj = {
+//     method1: function (name) {
+//        return this.name = name
+//     },
+//     method2: () => {
+//         return this
+//     }
+// };
+//
+// const obj2 = {
+//     name: "Gor",
+//     age: 17,
+// };
+//
+// console.log(new obj.method1(obj2.name))
